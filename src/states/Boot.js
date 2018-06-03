26ed5189033516371d78d35e68cc2fae756bb98f
Rick Gijsen <rs.gijsen@gmail.com>
2018-06-03 14:48:56 +0200

    Font fix attempt

--- src/states/Boot.js
+++ src/states/Boot.js
@@ -12,7 +12,7 @@ export default class extends Phaser.State {
   preload() {
     WebFont.load({
       google: {
-        families: ['Jelly Donuts'],
+        families: ['Bangers'],
       },
       active: this.fontsLoaded,
     });
